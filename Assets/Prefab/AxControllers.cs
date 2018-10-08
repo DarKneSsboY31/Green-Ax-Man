87fbcb7bc86ebf154816816b2fa1a92e3693960b
DarKneSsboY31 <blackimpact0123@outlook.jp>
2018-10-08 18:10:42 +0900

    Green Ax Man プロジェクト　初回コミット
    ゲーム制作を始めました。よろしくお願いします。

---
+++ Assets/Prefab/AxControllers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxControllers : MonoBehaviour
+{
+
+    //進むスピード
+    private float speed = 3.0f;
+    //Ax Manのアニメーションを取得
+    public Animator AxMans;
+    //AxManを取得
+    public GameObject AxMan;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //AxManが右向きの時、右へ進ませる,左の時は左へ進ませる
+        if (AxMans.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Ax Man Default"))
+        {
+            this.transform.Translate(speed,0,0);
+            Debug.Log("aaa");
+        }
+        else if (AxMans.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Walk Re") || AxMans.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Default Re"))
+        {
+            this.transform.Translate(-speed, 0, 0);
+            Debug.Log("bbb");
+        }
+
+        //一定の距離を超えたら、消す
+        if (this.transform.position.x > AxMan.transform.position.x + 5)
+        {
+            Destroy(gameObject);
+            Debug.Log("ccc");
+        }
+        else if (this.transform.position.x < AxMan.transform.position.x - 5)
+        {
+            Destroy(gameObject);
+            Debug.Log("ddd");
+        }
+
+    }
+}
